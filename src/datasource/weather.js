e81322ef1fa037cd54c316826ee24b020aaae66c
Alex Mireles <anchises1968@gmail.com>
2020-03-13 16:13:50 -0700

    updated weather.js

--- src/datasource/weather.js
+++ src/datasource/weather.js
@@ -11,7 +11,6 @@ class AgroAPI extends RESTDataSource {
 
   async getCurrentWeather() {
     const response = await this.get(`weather?polyid=5e66f15ff6e0ca64d7708957&appid=${API_KEY}`)
-    console.log(response)
     return this.weatherReducer(response)
   }
 
